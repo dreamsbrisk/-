c9608832f4ec8a89657c39f5340e8db56d97a92c
梦镜轻灵 <41447899+dreamsbrisk@users.noreply.github.com>
2020-04-03 21:51:32 +0800

    模板
    必备文件

---
+++ 模板.js
@@ -0,0 +1,46 @@
+setScreenMetrics(1080, 2340);
+
+function C5()
+    press(760,100,1);//低音1
+function D5()
+    press(960,100,1);//低音2
+function E5()
+    press(1160,100,1);//低音3
+function F5()
+    press(1360,100,1);//低音4
+function G5()
+	press(1560,100,1);//低音5
+function A5()
+	press(760,300,1);//低音6
+function B5()
+	press(960,300,1);//低音7
+function C6()
+	press(1160,300,1);//中音1
+function D6()
+	press(1360,300,1);//中音2
+function E6()
+	press(1560,300,1);//中音3
+function F6()
+	press(760,500,1);//中音4
+function G6()
+	press(960,500,1);//中音5
+function A6()
+	press(1160,500,1);//中音6
+function B6()
+	press(1360,500,1);//中音7
+function C7()
+	press(1560,500,1);//高音1
+
+//BPM:100
+function R1()
+	sleep(1000)
+function R2()
+	sleep(500)
+function R4()//四分之一拍
+	sleep(250)
+function R8()
+	sleep(250)
+function R16()
+	sleep(125)
+function R32()
+	sleep(62)
